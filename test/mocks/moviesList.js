8ded625bf0a52a9eb67c317f9a9e170985660f0a
FreddyJR1995 <freddy.valverde@epn.edu.ec>
2024-03-19 22:52:22 -0500

    test: [cin-4] add unit test for get movies function

---
+++ test/mocks/moviesList.js
@@ -0,0 +1,425 @@
+export const mockMoviesResponse = {
+    "entries": 10,
+    "results": [
+        {
+            "_id": "61e5807f78c012aa344139e5",
+            "id": "tt0040897",
+            "primaryImage": {
+                "id": "rm2805550592",
+                "width": 1807,
+                "height": 2724,
+                "url": "https://m.media-amazon.com/images/M/MV5BOTJlZWMxYzEtMjlkMS00ODE0LThlM2ItMDI3NGQ2YjhmMzkxXkEyXkFqcGdeQXVyMDI2NDg0NQ@@._V1_.jpg",
+                "caption": {
+                    "plainText": "Humphrey Bogart, Tim Holt, and Walter Huston in The Treasure of the Sierra Madre (1948)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "The Treasure of the Sierra Madre",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "The Treasure of the Sierra Madre",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1948,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 24,
+                "month": 1,
+                "year": 1948,
+                "__typename": "ReleaseDate"
+            },
+            "position": 103
+        },
+        {
+            "_id": "61e58083b597b91eb9413af7",
+            "id": "tt0042041",
+            "primaryImage": {
+                "id": "rm757335552",
+                "width": 960,
+                "height": 1440,
+                "url": "https://m.media-amazon.com/images/M/MV5BZmI5NTA3MjItYzdhMi00MWMxLTg3OWMtYWQyYjg5MTFmM2U0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
+                "caption": {
+                    "plainText": "James Cagney in White Heat (1949)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "White Heat",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "White Heat",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1949,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 3,
+                "month": 9,
+                "year": 1949,
+                "__typename": "ReleaseDate"
+            },
+            "position": 189
+        },
+        {
+            "_id": "61e5817bb597b91eb94166b1",
+            "id": "tt0110912",
+            "primaryImage": {
+                "id": "rm1959546112",
+                "width": 1055,
+                "height": 1536,
+                "url": "https://m.media-amazon.com/images/M/MV5BNGNhMDIzZTUtNTBlZi00MTRlLWFjM2ItYzViMjE3YzI5MjljXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+                "caption": {
+                    "plainText": "Uma Thurman in Pulp Fiction (1994)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Pulp Fiction",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Pulp Fiction",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1994,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 1,
+                "month": 12,
+                "year": 1994,
+                "__typename": "ReleaseDate"
+            },
+            "position": 9
+        },
+        {
+            "_id": "61e5812ed8f3c0931e415871",
+            "id": "tt0087843",
+            "primaryImage": {
+                "id": "rm480514304",
+                "width": 910,
+                "height": 1422,
+                "url": "https://m.media-amazon.com/images/M/MV5BMGFkNWI4MTMtNGQ0OC00MWVmLTk3MTktOGYxN2Y2YWVkZWE2XkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+                "caption": {
+                    "plainText": "Once Upon a Time in America (1984)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Once Upon a Time in America",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Once Upon a Time in America",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1984,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 27,
+                "month": 9,
+                "year": 1984,
+                "__typename": "ReleaseDate"
+            },
+            "position": 65
+        },
+        {
+            "_id": "61e58192d8f3c0931e416ad1",
+            "id": "tt0118715",
+            "primaryImage": {
+                "id": "rm318364928",
+                "width": 932,
+                "height": 1390,
+                "url": "https://m.media-amazon.com/images/M/MV5BMTQ0NjUzMDMyOF5BMl5BanBnXkFtZTgwODA1OTU0MDE@._V1_.jpg",
+                "caption": {
+                    "plainText": "Julianne Moore and Jeff Bridges in The Big Lebowski (1998)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "The Big Lebowski",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "The Big Lebowski",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1998,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 14,
+                "month": 5,
+                "year": 1998,
+                "__typename": "ReleaseDate"
+            },
+            "position": 163
+        },
+        {
+            "_id": "61e580cd95801a78bc41478d",
+            "id": "tt0061512",
+            "primaryImage": {
+                "id": "rm956369152",
+                "width": 975,
+                "height": 1467,
+                "url": "https://m.media-amazon.com/images/M/MV5BOWFlNzZhYmYtYTI5YS00MDQyLWIyNTUtNTRjMWUwNTEzNjA0XkEyXkFqcGdeQXVyNjUwNzk3NDc@._V1_.jpg",
+                "caption": {
+                    "plainText": "Paul Newman in Cool Hand Luke (1967)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Cool Hand Luke",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Cool Hand Luke",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1967,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 1,
+                "month": 11,
+                "year": 1967,
+                "__typename": "ReleaseDate"
+            },
+            "position": 181
+        },
+        {
+            "_id": "61e5809c8a5a6a599f413ebd",
+            "id": "tt0048424",
+            "primaryImage": {
+                "id": "rm3485546496",
+                "width": 977,
+                "height": 1455,
+                "url": "https://m.media-amazon.com/images/M/MV5BYTNjN2M2MzYtZGEwMi00Mzc5LWEwYTMtODM1ZmRiZjFiNTU0L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjc1NTYyMjg@._V1_.jpg",
+                "caption": {
+                    "plainText": "The Night of the Hunter (1955)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "The Night of the Hunter",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "The Night of the Hunter",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1955,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 10,
+                "month": 11,
+                "year": 1956,
+                "__typename": "ReleaseDate"
+            },
+            "position": 241
+        },
+        {
+            "_id": "61e5810a971a4a15d64151ed",
+            "id": "tt0078748",
+            "primaryImage": {
+                "id": "rm2990766080",
+                "width": 1984,
+                "height": 2922,
+                "url": "https://m.media-amazon.com/images/M/MV5BMmQ2MmU3NzktZjAxOC00ZDZhLTk4YzEtMDMyMzcxY2IwMDAyXkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_.jpg",
+                "caption": {
+                    "plainText": "Alien (1979)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Alien",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Alien",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1979,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 25,
+                "month": 10,
+                "year": 1979,
+                "__typename": "ReleaseDate"
+            },
+            "position": 41
+        },
+        {
+            "_id": "61e5812bef99b1c54341578b",
+            "id": "tt0087884",
+            "primaryImage": {
+                "id": "rm3886030336",
+                "width": 1138,
+                "height": 1755,
+                "url": "https://m.media-amazon.com/images/M/MV5BM2RjMmU3ZWItYzBlMy00ZmJkLWE5YzgtNTVkODdhOWM3NGZhXkEyXkFqcGdeQXVyNDA5Mjg5MjA@._V1_.jpg",
+                "caption": {
+                    "plainText": "Nastassja Kinski, Harry Dean Stanton, and Hunter Carson in Paris, Texas (1984)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Paris, Texas",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Paris, Texas",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 1984,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 8,
+                "month": 11,
+                "year": 1984,
+                "__typename": "ReleaseDate"
+            },
+            "position": 190
+        },
+        {
+            "_id": "61e5bca0b597b91eb94fa9a7",
+            "id": "tt4154756",
+            "primaryImage": {
+                "id": "rm4044245504",
+                "width": 1382,
+                "height": 2048,
+                "url": "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_.jpg",
+                "caption": {
+                    "plainText": "Don Cheadle, Robert Downey Jr., Josh Brolin, Vin Diesel, Paul Bettany, Bradley Cooper, Chris Evans, Scarlett Johansson, Elizabeth Olsen, Chris Pratt, Mark Ruffalo, Zoe Saldana, Benedict Wong, Anthony Mackie, Chris Hemsworth, Dave Bautista, Benedict Cumberbatch, Chadwick Boseman, Sebastian Stan, Danai Gurira, Karen Gillan, Pom Klementieff, Letitia Wright, and Tom Holland in Avengers: Infinity War (2018)",
+                    "__typename": "Markdown"
+                },
+                "__typename": "Image"
+            },
+            "titleType": {
+                "text": "Movie",
+                "id": "movie",
+                "isSeries": false,
+                "isEpisode": false,
+                "__typename": "TitleType"
+            },
+            "titleText": {
+                "text": "Avengers: Infinity War",
+                "__typename": "TitleText"
+            },
+            "originalTitleText": {
+                "text": "Avengers: Infinity War",
+                "__typename": "TitleText"
+            },
+            "releaseYear": {
+                "year": 2018,
+                "endYear": null,
+                "__typename": "YearRange"
+            },
+            "releaseDate": {
+                "day": 25,
+                "month": 4,
+                "year": 2018,
+                "__typename": "ReleaseDate"
+            },
+            "position": 51
+        }
+    ]
+}
